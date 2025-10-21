86078c5245524aeb08d0250b2373c8f48359693c
tesinitsyn <sitiev174@gmail.com>
2025-10-21 11:47:22 +0300

    fix packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,22 +7,25 @@ group = "org.mlgym"
 version = "1.0.1" // при изменении DTO повышай версию
 
 java {
+    withJavadocJar()
     withSourcesJar()
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
+
 repositories {
     mavenCentral()
 }
-
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            groupId = "org.mlgym"
             artifactId = "common"
+            version = "1.0.2"
         }
     }
 
@@ -31,12 +34,15 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/New-Fitness/common")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("PACKAGE_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("PACKAGE_TOKEN")
+                username = project.findProperty("gpr.user") as String?
+                    ?: System.getenv("PACKAGE_ACTOR")
+                password = project.findProperty("gpr.key") as String?
+                    ?: System.getenv("PACKAGE_TOKEN")
             }
         }
     }
 }
+
 
 dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
